cd7a89aca94f24368617ead66c9dbf6830a805f1
sauravsarkar100 <70365386+sauravsarkar100@users.noreply.github.com>
2020-10-01 19:46:15 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+    $('#down').click(function () { 
+        $('.chck').slideToggle('slow');
+        
+    });
+
+    $('#gol').click(function () { 
+        $('#cod').slideToggle('slow');
+        
+    });
+
+    $('#price').click(function () { 
+        $('.dolor').slideToggle('slow');
+        
+    });
+
+    $('.hide').click(function () { 
+        $('.mobil').toggle('slow');
+        
+    });
+
+});
